cb212734c6015e561ff371ec950c36d0535d9bc4
Karthik Sunkari <84893245+Karthiksunkari3@users.noreply.github.com>
2023-05-26 23:19:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 const parseCode = (str) => {
-  // your code here
+  const [firstName, lastName, id] = str.split('0');
+  return { firstName, lastName, id };
 };
+console.log(parseCode("John000Doe000123"));
+// Output: { firstName: "John", lastName: "Doe", id: "123" }
 
-// Do not change the code below
-const str = prompt("Enter str: ");
-alert(JSON.stringify(parseCode(str)));
+console.log(parseCode("michael0smith004331"));
+// Output: { firstName: "michael", lastName: "smith", id: "4331" }
+
+console.log(parseCode("Thomas00LEE0000043"));
+// Output: { firstName: "Thomas", lastName: "LEE", id: "43" }
